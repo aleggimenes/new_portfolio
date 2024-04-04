10445d5c23a02bab91b07c99540030dbda2c1ec3
Alessandro <alessandro.alessandrogimenes@gmail.com>
2024-04-04 22:45:53 +0100

    [feat]-new-update-hero-component

--- src/pages/Hero/index.tsx
+++ src/pages/Hero/index.tsx
@@ -1,65 +1,82 @@
-import React, { ReactNode } from 'react';
-import { useSpring, animated } from '@react-spring/web';
-import { useDrag } from 'react-use-gesture';
+import React from 'react';
+import { useSpring } from 'react-spring';
+import { ParallaxProvider } from "react-scroll-parallax";
 import './style.css';
-
-const left = {
-    background: `linear-gradient(120deg, #f093fb 0%, #f5576c 100%)`,
-    justifySelf: 'center',
-};
-const right = {
-    background: `linear-gradient(120deg, #96fbc4 0%, #f9f586 100%)`,
-    justifySelf: 'center',
-};
-
-const Slider: React.FC<{ children: ReactNode }> = ({ children }) => {
-    const [{ x, background, scale, justifySelf }, api] = useSpring(() => ({
-        x: 0,
-        scale: 1,
-        ...left,
-    }));
-
-    const bind = useDrag(({ active, movement: [mx] }) =>
-        api.start({
-            x: active ? mx : 0,
-            scale: active ? 1.1 : 1,
-            ...(mx < 0 ? left : right),
-            immediate: (name) => active && name === 'x',
-        })
-    );
-
-    const avSize = x.to({
-        map: Math.abs,
-        range: [50, 300],
-        output: [0.5, 1],
-        extrapolate: 'clamp',
-    });
-
-    // Verifica se o slider está puxado para a esquerda
-    const isLeft = x.to((value) => value < 0);
-
-    return (
-        <animated.div {...bind()} className='item' style={{ background }}>
-            <animated.div className='av' style={{ scale: avSize, justifySelf }}>
-                <animated.p style={{ color: 'black', fontSize: 34, lineHeight: '1.5' }}>
-                    Hey, I'm Alessandro <br />
-                    Passionate about crafting innovative solutions that make a
-                    difference.
-                </animated.p>
-            </animated.div>
-            <animated.div className='fg' style={{ x, scale }}>
-                {children}
-            </animated.div>
-        </animated.div>
-    );
-};
-
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 export default function Hero() {
-    return (
-        <div className='container'>
-            <h1>Slide to see more</h1>
-            <Slider>About Me</Slider>
+    const [ref1, inView1] = useInView({ threshold: 0.5 }); // Ref e estado para o primeiro componente
+    const [ref2, inView2] = useInView({ threshold: 0.5 }); // Ref e estado para o segundo componente
+    const [ref3, inView3] = useInView({ threshold: 0.5 }); // Ref e estado para o terceiro componente
+    const [ref4, inView4] = useInView({ threshold: 0.5 }); // Ref e estado para o segundo componente
 
-        </div>
+    const containerVariants = {
+        hidden: { opacity: 0, y: 50 },
+        visible: { opacity: 1, y: 0, transition: { duration: 0.5 } }
+    };
+    const [{ height }, setHeight] = useSpring(() => ({ height: '0%' }));
+
+    // Função para controlar a altura da imagem com base no valor de scrollTop
+    const handleScroll = () => {
+        const scrollPosition = window.pageYOffset;
+        const scrollHeight = document.body.scrollHeight - window.innerHeight;
+        const scrollPercentage = (scrollPosition / scrollHeight) * 100;
+        setHeight({ height: `${scrollPercentage}%` });
+    };
+
+    // Adiciona um observador de rolagem ao componente
+    React.useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return (
+        <ParallaxProvider>
+            <div className='hero-section-laptop-container'>
+                <img src="./img/rocket.png" alt="" style={{ width: '25%', paddingTop: '120px', zIndex: 2 }} />
+                <div className='contain-info'>
+                    <p className="contain-info-title">Hey, I'm Alessandro</p>
+                    <h1 className="contain-info-subtitle">
+                        Full Stack
+                        Developer
+                    </h1>
+                    <motion.p className="contain-info-text"
+                        variants={containerVariants}
+                        initial="hidden"
+                        animate={inView1 ? "visible" : "hidden"}
+                        ref={ref1}
+                    >
+                        Passionate about crafting innovative solutions that make a
+                        difference.
+                    </motion.p>
+                    <motion.p className="contain-info-text"
+                    variants={containerVariants}
+                    initial="hidden"
+                    animate={inView2 ? "visible" : "hidden"}
+                    ref={ref2}>
+                        With a blend of creativity and technical expertise,
+
+                    </motion.p>
+                    <motion.p className="contain-info-text"
+                    variants={containerVariants}
+                    initial="hidden"
+                    animate={inView3 ? "visible" : "hidden"}
+                    ref={ref3}>
+                        I excel at making ideas real and pushing digital boundaries.
+                    </motion.p>
+                    <motion.p className="contain-info-text"
+                    variants={containerVariants}
+                    initial="hidden"
+                    animate={inView4 ? "visible" : "hidden"}
+                    ref={ref4}>
+                        Let's build something amazing together!
+
+                    </motion.p>
+                </div>
+
+            </div>
+        </ParallaxProvider>
     );
 }
--- src/pages/Skills/index.tsx
+++ src/pages/Skills/index.tsx
@@ -10,14 +10,20 @@ export default function Skills() {
     })
     return (
         <div className="skills-section">
-            <h1 style={{ fontSize: '35px', color: 'white', marginBottom:'40px' }}>Click to see more</h1>
+            <h1 style={{ fontSize: '35px', color: 'white', marginBottom: '40px' }}>Click to see more</h1>
             <div className='container' onClick={() => set(state => !state)}>
                 <a.div
                     className='c back'
                     style={{ opacity: opacity.to(o => 1 - o), transform }}
                 >
+                    <div style={{ width: '90%', display: 'flex', padding: '20px', flexDirection: 'row', gap: '22px', justifyContent: 'flex-start', alignItems: 'flex-start', margin: '0 auto' }}>
+                        <div className="small-circle-red" />
+                        <div className="small-circle-yellow" />
+                        <div className="small-circle-green" />
+                    </div>
+                    <div style={{ width: '90%', border: '1px solid  #161616', margin: '0 auto' }} />
                     <div style={{ textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                        <h1 style={{ lineHeight: '74.4px', fontWeight: '500', fontSize: '62px', marginBottom: '40px' }}>Backend</h1>
+                        <h1 style={{ lineHeight: '74.4px', fontWeight: '500', fontSize: '62px', marginBottom: '40px', marginTop: '80px' }}>Backend</h1>
                         <p style={{ lineHeight: '28px', fontSize: '20px', width: '80%', textAlign: 'center', letterSpacing: '0.7px' }}>
                             Using technologies like NodeJS and AdonisJS, I excel at building efficient APIs and completing data processing tasks seamlessly. With in-depth knowledge of database administration, specifically MySQL, I ensure the integrity and security of data storage and retrieval processes.
                         </p>
@@ -31,8 +37,15 @@ export default function Skills() {
                         rotateX: '180deg',
                     }}
                 >
+                    <div style={{ width: '90%', display: 'flex', padding: '20px', flexDirection: 'row', gap: '22px', justifyContent: 'flex-start', alignItems: 'flex-start', margin: '0 auto' }}>
+                        <div className="small-circle-red" />
+                        <div className="small-circle-yellow" />
+                        <div className="small-circle-green" />
+                    </div>
+                    <div style={{ width: '90%', border: '1px solid  #cdcdcd', margin: '0 auto' }} />
+
                     <div style={{ textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                        <h1 style={{ lineHeight: '74.4px', fontWeight: '500', fontSize: '62px', marginBottom: '40px' }}>Frontend</h1>
+                        <h1 style={{ lineHeight: '74.4px', fontWeight: '500', fontSize: '62px', marginBottom: '40px', marginTop: '80px' }}>Frontend</h1>
                         <p style={{ lineHeight: '28px', fontSize: '20px', width: '80%', textAlign: 'center', letterSpacing: '0.7px' }}>
                             My toolkit includes ReactJS, NextJS, and Chakra-ui, enabling me to build dynamic and responsive web applications tailored to meet diverse client needs.
                         </p>
