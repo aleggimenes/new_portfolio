d36fac23ef41e8ca1b73e9ca13d86bb0779b8c67
Alessandro <alessandro.alessandrogimenes@gmail.com>
2024-04-05 12:45:04 +0100

    [new]-new-styles-hero

--- src/pages/Hero/index.tsx
+++ src/pages/Hero/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { useSpring } from 'react-spring';
-import { ParallaxProvider } from "react-scroll-parallax";
+import { useSpring, animated } from 'react-spring';
+import { ParallaxProvider, Parallax } from "react-scroll-parallax";
 import './style.css';
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 export default function Hero() {
-    const [ref1, inView1] = useInView({ threshold: 0.5 }); // Ref e estado para o primeiro componente
-    const [ref2, inView2] = useInView({ threshold: 0.5 }); // Ref e estado para o segundo componente
-    const [ref3, inView3] = useInView({ threshold: 0.5 }); // Ref e estado para o terceiro componente
-    const [ref4, inView4] = useInView({ threshold: 0.5 }); // Ref e estado para o segundo componente
+    const [ref1, inView1] = useInView({ threshold: 0.5 });
+    const [ref2, inView2] = useInView({ threshold: 0.5 }); 
+    const [ref3, inView3] = useInView({ threshold: 0.5 }); 
+    const [ref4, inView4] = useInView({ threshold: 0.5 }); 
+    const [ref5, inView5] = useInView({ threshold: 0.5 }); 
+
 
     const containerVariants = {
         hidden: { opacity: 0, y: 50 },
@@ -16,7 +18,6 @@ export default function Hero() {
     };
     const [{ height }, setHeight] = useSpring(() => ({ height: '0%' }));
 
-    // Função para controlar a altura da imagem com base no valor de scrollTop
     const handleScroll = () => {
         const scrollPosition = window.pageYOffset;
         const scrollHeight = document.body.scrollHeight - window.innerHeight;
@@ -24,24 +25,44 @@ export default function Hero() {
         setHeight({ height: `${scrollPercentage}%` });
     };
 
-    // Adiciona um observador de rolagem ao componente
     React.useEffect(() => {
         window.addEventListener('scroll', handleScroll);
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
     }, []);
+    const [{ xy }, set] = useSpring(() => ({ xy: [0, 0] }));
+
 
     return (
         <ParallaxProvider>
+            <div className="hero-top-fade-top" />
             <div className='hero-section-laptop-container'>
-                <img src="./img/rocket.png" alt="" style={{ width: '25%', paddingTop: '120px', zIndex: 2 }} />
+                <animated.img src="./img/rocket.png" alt="" style={{ width: '12%', paddingTop: '120px', zIndex: 2 }} />
+                <Parallax translateY={[10, 190]} style={{ width: '100%' }}>
+                    <img src="https://awv3node-homepage.surge.sh/build/assets/satellite4.svg" alt="" style={{ width: '8%', paddingTop: '20px', zIndex: 2 }} />
+                </Parallax>
+                <Parallax style={{ position: 'absolute', top: '1000px', width: '100%' }}
+                    translateY={[10, 190]}>
+                    <img src="https://awv3node-homepage.surge.sh/build/assets/cloud.svg" alt="" style={{ width: '8%', paddingTop: '20px', zIndex: 2, }} />
+                </Parallax>
+                <Parallax style={{ position: 'absolute', top: '1800px', width: '100%', display: 'flex', alignItems: 'center' }}
+                    translateY={[10, 190]}>
+                    <img src="https://awv3node-homepage.surge.sh/build/assets/cloud.svg" alt="" style={{ width: '8%', paddingTop: '20px', zIndex: 2, }} />
+                </Parallax>
                 <div className='contain-info'>
-                    <p className="contain-info-title">Hey, I'm Alessandro</p>
-                    <h1 className="contain-info-subtitle">
-                        Full Stack
-                        Developer
-                    </h1>
+                    <motion.div
+                        variants={containerVariants}
+                        initial="hidden"
+                        animate={inView5 ? "visible" : "hidden"}
+                        ref={ref5}
+                    >
+                        <p className="contain-info-title">Hey, I'm Alessandro</p>
+                        <h1 className="contain-info-subtitle">
+                            Full Stack
+                            Developer
+                        </h1>
+                    </motion.div>
                     <motion.p className="contain-info-text"
                         variants={containerVariants}
                         initial="hidden"
@@ -52,30 +73,42 @@ export default function Hero() {
                         difference.
                     </motion.p>
                     <motion.p className="contain-info-text"
-                    variants={containerVariants}
-                    initial="hidden"
-                    animate={inView2 ? "visible" : "hidden"}
-                    ref={ref2}>
+                        variants={containerVariants}
+                        initial="hidden"
+                        animate={inView2 ? "visible" : "hidden"}
+                        ref={ref2}>
                         With a blend of creativity and technical expertise,
 
                     </motion.p>
                     <motion.p className="contain-info-text"
-                    variants={containerVariants}
-                    initial="hidden"
-                    animate={inView3 ? "visible" : "hidden"}
-                    ref={ref3}>
+                        variants={containerVariants}
+                        initial="hidden"
+                        animate={inView3 ? "visible" : "hidden"}
+                        ref={ref3}>
                         I excel at making ideas real and pushing digital boundaries.
                     </motion.p>
                     <motion.p className="contain-info-text"
-                    variants={containerVariants}
-                    initial="hidden"
-                    animate={inView4 ? "visible" : "hidden"}
-                    ref={ref4}>
+                        variants={containerVariants}
+                        initial="hidden"
+                        animate={inView4 ? "visible" : "hidden"}
+                        ref={ref4}>
                         Let's build something amazing together!
 
                     </motion.p>
                 </div>
-
+                {/* <img
+                    src="https://awv3node-homepage.surge.sh/build/assets/earth.svg"
+                    alt=""
+                    style={{
+                        position: 'absolute',
+                        width: '38%',
+                        paddingBottom: '120px', 
+                        zIndex: 1,
+                        bottom: 0, 
+                        overflow: 'hidden',
+                        clipPath: 'inset(0px 0px 50% 0px)'
+                    }}
+                /> */}
             </div>
         </ParallaxProvider>
     );
